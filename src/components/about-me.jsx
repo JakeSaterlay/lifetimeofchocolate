c13af2108a9fe4d265129632a89d6e340f40dd5a
Jake Saterlay <jake@vocala.co>
2022-04-19 21:37:43 +0100

    styling changes

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
         <div>
             <div class="section">
                 <div className="section-text">
-                    <h1>About me</h1>
+                    <p className="title">About me</p>
                     <p>
                         I started my career as a chef in Norwich, whilst at college I developed a passion for baking and patisserie. 
                         I then jumped at the chance to move to New York City and work as a baker for 8 months. I then returned to the UK and took the 
@@ -17,7 +17,7 @@ function AboutMe() {
                     </p>
                 </div>
                 <div className="section-img">
-                    <img src={chloe} className="" alt="chloe" />
+                    <img src={chloe} className="about-me-img" alt="chloe" />
                 </div>
             </div>
         </div>
