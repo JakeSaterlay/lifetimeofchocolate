51d63fac5b2650a0e2b3da08ccbd71c86ea0ea46
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2022-04-22 09:27:21 +0100

    font changes

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -6,8 +6,8 @@ function AboutMe() {
         <div>
             <div class="section">
                 <div className="section-text">
-                    <p className="title">Hey, I'm Chloe!</p>
-                    <p>
+                    <p className="section-title">Hey, I'm Chloe!</p>
+                    <p className="text">
                         I started my career as a chef in Norwich, whilst at college I developed a passion for baking and patisserie. 
                         I then jumped at the chance to move to New York City and work as a baker for 8 months. I then returned to the UK and took the 
                         opportunity to work in some of the most inspired kitchens such as Le Manior Aux Quat Saisons and William Curley. 
