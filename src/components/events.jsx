eeee449d432e15205cd08598a14e03ed85be88b8
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2022-04-29 14:29:35 +0100

    First publish

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,21 +1,22 @@
 import "../styles/layout.css";
 
 function Events() {
-	return (
-		<div className="section events-section">
-			<div className="section-text">
-				<p className="section-title">Upcoming Events</p>
-				<p className="text">You can find us at the following very soon!</p>
-				<div className="events-list">
-					<li>Place 1</li>
-					<li>Place 2</li>
-					<li>Place 3</li>
-					<li>Place 4</li>
-					<li>Place 5</li>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="section events-section">
+      <div className="section-text">
+        <p className="section-title">Upcoming Events</p>
+        <p className="text">You can find us at the following very soon!</p>
+        <div className="events-list">
+          <div>1st May - Holt Market</div>
+          <div>13th May - Black Barn Farm Salhouse</div>
+          <div>15th May - Diss Organ Festival</div>
+          <div>5th June - Holt Market </div>
+          <div>29-30th June - Royal Norfolk Show</div>
+          <div>6th August - Tacolneston Village Hall</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Events;
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -7,43 +7,43 @@ import pic3 from "../images/pic3.jpeg";
 import chloe from "../images/chloe.jpg";
 
 function Products() {
-	return (
-		<div className="section products-section">
-			<div className="section-img">
-				<Carousel
-					className="product-carousel"
-					showThumbs={false}
-					showStatus={false}
-					autoPlay={true}
-					infiniteLoop={true}
-				>
-					<div>
-						<img style={{ height: "600px" }} src={pic1} />
-					</div>
-					<div>
-						<img style={{ height: "600px" }} src={pic2} />
-					</div>
-					<div>
-						<img style={{ height: "600px" }} src={pic3} />
-					</div>
-				</Carousel>
-			</div>
-			<div className="section-text">
-				<p className="section-title">Our Tasty Treats</p>
-				<p className="text">
-					I started my career as a chef in Norwich, whilst at college I
-					developed a passion for baking and patisserie. I then jumped at the
-					chance to move to New York City and work as a baker for 8 months. I
-					then returned to the UK and took the opportunity to work in some of
-					the most inspired kitchens such as Le Manior Aux Quat Saisons and
-					William Curley. I decided my passion is in chocolate so I moved to
-					London, to work for Paul A Young Fine Chocolates, for 15 months before
-					deciding to come back to my home in Norwich and set up a small
-					business making wonderful handmade chocolates and treats!
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="section products-section">
+      <div className="section-img">
+        <Carousel
+          className="product-carousel"
+          showThumbs={false}
+          showStatus={false}
+          autoPlay={true}
+          infiniteLoop={true}
+        >
+          <div>
+            <img style={{ height: "600px" }} src={pic1} />
+          </div>
+          <div>
+            <img style={{ height: "600px" }} src={pic2} />
+          </div>
+          <div>
+            <img style={{ height: "600px" }} src={pic3} />
+          </div>
+        </Carousel>
+      </div>
+      <div className="section-text">
+        <p className="section-title">Our Tasty Treats</p>
+        <p className="text">
+          I started my career as a chef in Norwich, whilst at college I
+          developed a passion for baking and patisserie. I then jumped at the
+          chance to move to New York City and work as a baker for 8 months. I
+          then returned to the UK and took the opportunity to work in some of
+          the most inspired kitchens such as Le Manior Aux Quat Saisons and
+          William Curley. I decided my passion is in chocolate so I moved to
+          London, to work for Paul A Young Fine Chocolates, for 15 months before
+          deciding to come back to my home in Norwich and set up a small
+          business making wonderful handmade chocolates and treats!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Products;
